0fa0852b70415a4c725981792d4984a627414254
Hridoy3519 <hridoy3519@gmail.com>
2022-08-03 23:05:11 +0600

    CRUD Operations Done

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "";
+    $dbName = "yum_burgers";
+
+
+    $conn = mysqli_connect($serverName, $userName, $password, $dbName);
+
+?>
